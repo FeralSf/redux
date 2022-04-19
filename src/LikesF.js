38adc13854064b173234d19a500087a665e32c65
FeralSf <EpicFailSf2012@gmail.com>
2022-04-19 10:38:36 +0300

    likes-f

--- src/LikesF.js
+++ src/LikesF.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const LikesF = () => {
-	return <div>LikesF</div>
+	return (
+		<div>
+			LikesF<div>LikesF</div>
+			<div>LikesF</div>
+			<div>LikesF</div>
+			<div>LikesF</div>
+		</div>
+	)
 }
 
 export default LikesF
