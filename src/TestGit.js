328556148b421104be5edd80434ad2ef94417dff
FeralSf <EpicFailSf2012@gmail.com>
2022-04-19 10:49:17 +0300

    testgit2

--- src/TestGit.js
+++ src/TestGit.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const TestGit = () => {
-	return <div>TestGit</div>
+	return (
+		<div>
+			TestGit<div>TestGit</div>
+			<div>TestGit</div>
+			<div>TestGit</div>
+			<div>TestGit</div>
+			<div>TestGit</div>
+			<div>TestGit</div>
+			<div>TestGit</div>
+		</div>
+	)
 }
 
 export default TestGit
